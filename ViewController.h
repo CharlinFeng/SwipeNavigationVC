7a664b8e433258eaa445baa2fb9146c7cd1a9131
nsdictionary <huaxi100@muxi.local>
2015-02-09 11:21:46 +0800

    向右滑动退出栈顶控制器
    向右滑动退出栈顶控制器

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwipeNavigationVC
+//
+//  Created by muxi on 15/2/9.
+//  Copyright (c) 2015年 muxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
